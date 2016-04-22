6bc35ad4227dc8e45804ad2131c9d8eff87b0b5c
Matthias Beyer <mail@beyermatthias.de>
2016-04-22 17:10:50 +0200

    Add main.rs

--- imag-diary/src/main.rs
+++ imag-diary/src/main.rs
@@ -6,6 +6,102 @@ extern crate libimagrt;
 extern crate libimagdiary;
 extern crate libimagutil;
 
+use std::process::exit;
+
+use libimagrt::edit::Edit;
+use libimagrt::runtime::Runtime;
+use libimagdiary::diary::Diary;
+use libimagdiary::error::DiaryError as DE;
+use libimagdiary::error::DiaryErrorKind as DEK;
+use libimagutil::trace::trace_error;
+
+mod ui;
+
+use ui::build_ui;
+
 fn main() {
-    println!("Hello, world!");
+    let name = "imag-diary";
+    let version = &version!()[..];
+    let about = "Personal Diary/Diaries";
+    let ui = build_ui(Runtime::get_default_cli_builder(name, version, about));
+    let rt = {
+        let rt = Runtime::new(ui);
+        if rt.is_ok() {
+            rt.unwrap()
+        } else {
+            println!("Could not set up Runtime");
+            println!("{:?}", rt.err().unwrap());
+            exit(1);
+        }
+    };
+
+    rt.cli()
+        .subcommand_name()
+        .map(|name| {
+            debug!("Call {}", name);
+            match name {
+                "create" => create(&rt),
+                // "delete" => delete(&rt),
+                // "edit" => edit(&rt),
+                "list" => list(&rt),
+                "diary" => diary(&rt),
+                _        => {
+                    debug!("Unknown command"); // More error handling
+                },
+            }
+        });
+}
+
+fn create(rt: &Runtime) {
+    let diaryname = get_diary_name(rt);
+    if diaryname.is_none() {
+        warn!("No diary selected. Use either the configuration file or the commandline option");
+        exit(1);
+    }
+    let diaryname = diaryname.unwrap();
+
+    let prevent_edit = rt.cli().subcommand_matches("create").unwrap().is_present("no-edit");
+
+    let diary = Diary::open(rt.store(), &diaryname[..]);
+    let res = diary.new_entry_today()
+        .and_then(|mut entry| {
+            if prevent_edit {
+                debug!("Not editing new diary entry");
+                Ok(())
+            } else {
+                debug!("Editing new diary entry");
+                entry.edit_content(rt)
+                    .map_err(|e| DE::new(DEK::DiaryEditError, Some(Box::new(e))))
+            }
+        });
+
+    if let Err(e) = res {
+        trace_error(&e);
+    } else {
+        info!("Ok!");
+    }
+}
+
+fn list(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn delete(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn edit(rt: &Runtime) {
+    unimplemented!()
+}
+
+fn diary(rt: &Runtime) {
+    unimplemented!()
+}
+
+
+fn get_diary_name(rt: &Runtime) -> Option<String> {
+    use libimagdiary::config::get_default_diary_name;
+
+    get_default_diary_name(rt)
+        .or(rt.cli().value_of("diaryname").map(String::from))
 }
